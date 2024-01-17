c672d1bcfd8a88c05e5ca9afeb147a7c0c5811fd
FirasCh3 <firaschebil61@gmail.com>
2024-01-17 19:29:20 +0100

    calculated derivative and added gradient descent, added confusion matrix and accuracy and the plot for the loss function

--- Neural network.py
+++ Neural network.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 class LogisticRegressor:
+
     def __init__(self, epochs=10, alpha=0.01):
+        self.epochs = epochs
+        self.alpha = alpha
         self.X = None
         self.y = None
         self.x_train = None
@@ -25,69 +29,90 @@ class LogisticRegressor:
         return 1 * (Z > 0)
     def __derivative(self, Z):
 
-        return self.sigmoid(Z)*(1-self.sigmoid(Z))
+        return self.__sigmoid(Z)*(1-self.__sigmoid(Z))
     #using xavier weight init to avoid weight explosion or vanishing gradient
     def __weight_init(self, input_length, output_length):
-        np.random.seed(0)
+        np.random.seed(123)
         weight = np.random.normal(0, np.sqrt(2/(input_length+output_length)), (output_length, input_length))
         return weight
     def train(self):
-        epsilon = 1e-5
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.X, self.y, train_size=0.3, random_state=42)
+        epsilon = 1e-10
         self.biases = np.zeros(shape=(3, 1))
         self.biases2 = np.zeros(shape=(1, 1))
-        self.weights = self.__weight_init(7, 3)
+        self.weights = self.__weight_init(15, 3)
         self.weight2 = self.__weight_init(3, 1)
-        loss = 0
-        i = 0
-        for x, y in zip(self.x_train, self.y_train):
-            i += 1
-            x = np.reshape(x, (7, 1))
+        total_loss = []
+        correct_predictions = 0
+        for i in range(self.epochs):
+            loss = 0
+            for x, y in zip(self.x_train, self.y_train):
+                x = np.reshape(x, (15, 1))
+                layer1 = self.__relu(np.dot(self.weights, x)+self.biases)
+                output = self.__sigmoid(np.dot(self.weight2, layer1)+self.biases2)
+                if output >= 0.5:
+                    prediction = 1
+                else:
+                    prediction = 0
+
+                if prediction == y:
+                    correct_predictions += 1
+                loss += (-y * np.log(output+epsilon))-((1-y)*np.log(1-output+epsilon))
+                sigma = (output-y) * self.weight2.T
+                dw1 = x.T * sigma * self.__derivative_relu(np.dot(self.weights, x)+self.biases)
+                dw2 = (output-y) * layer1.T
+                db1 = sigma * self.__derivative_relu(np.dot(self.weights, x)+self.biases)
+                db2 = output-y
+                self.weights = self.weights - self.alpha * dw1
+                self.weight2 = self.weight2 - self.alpha * dw2
+                self.biases = self.biases - self.alpha * db1
+                self.biases2 = self.biases2 - self.alpha * db2
+            total_loss.append(loss[0, 0]/len(self.x_train))
+        print("training accuracy: "+str(correct_predictions/(self.epochs*len(self.x_train))))
+        plt.plot([i for i in range(self.epochs)], total_loss)
+        plt.show()
+
+    def predict(self):
+        correct_predictions = 0
+        total_predictions = 0
+        pred = []
+        for x, y in zip(self.x_test, self.y_test):
+            x = np.reshape(x, (15, 1))
             layer1 = self.__relu(np.dot(self.weights, x)+self.biases)
             output = self.__sigmoid(np.dot(self.weight2, layer1)+self.biases2)
-            loss += (-y * np.log(output+epsilon))+((1-y)*np.log(1-output+epsilon))
-            sigma = (-y+output)*self.weight2
-            print(self.weights.shape)
-            print(sigma.shape)
-            dw1 = sigma*np.dot(x, self.__derivative_relu(np.dot(self.weights, x)+self.biases).T)
-            print(np.dot(x, self.__derivative_relu(np.dot(self.weights, x)+self.biases).T).shape)
-            print(dw1.shape)
-            dw2 = -y*output*layer1
-            db1 = sigma.T*self.__derivative_relu(np.dot(self.weights, x)+self.biases)*self.biases
+            total_predictions += 1
+            if output >= 0.5:
+                output = 1
+            else:
+                output = 0
+            pred.append(output)
+            if output == y:
+                correct_predictions += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #def predict(self):
+        sns.heatmap(confusion_matrix(self.y_test, pred), annot=True)
+        plt.show()
+        print("model accuracy on test data: " + str(correct_predictions/total_predictions))
     def __normalize(self, df, column):
         df[column] = (df[column]-df.min()[column])/df.std()[column]
-    def pre_process(self,path):
+    def pre_process(self, path):
         df = pd.read_csv(path)
-        df = pd.get_dummies(df, columns=["island"], dtype=int)
-        df = df.replace({"Adelie": 1, "Gentoo": 0})
-        self.__normalize(df, "body_mass_g")
-        df = df.drop(columns=["year"])
-        self.y = np.array(df["species"][:10])
+        df["glucose"].fillna(value=df["glucose"].mean(), inplace=True)
+        df["BMI"].fillna(value=df["BMI"].mean(), inplace=True)
+        df["heartRate"].fillna(value=df["heartRate"].mean(), inplace=True)
+        df["totChol"].fillna(value=df["totChol"].mean(), inplace=True)
+        df["education"].fillna(value=df["education"].mean(), inplace=True)
+        df["cigsPerDay"].fillna(value=df["cigsPerDay"].mean(), inplace=True)
+        df["BPMeds"].fillna(value=df["BPMeds"].mean(), inplace=True)
+        for column in ["age", "cigsPerDay", "totChol", "sysBP", "diaBP", "BMI", "heartRate", "glucose"]:
+            self.__normalize(df, column)
+        self.y = np.array(df["TenYearCHD"])
+        self.X = np.array(df.drop(columns=["TenYearCHD"]))
 
-        self.X = np.array(df.drop(columns=["species"])[:10])
 
-
-reg = LogisticRegressor()
-reg.pre_process("./data/penguins_binary_classification.csv")
+reg = LogisticRegressor(epochs=100)
+reg.pre_process("./data/framingham.csv")
 reg.train()
+reg.predict()
 
 
 
